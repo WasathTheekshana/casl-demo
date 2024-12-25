d78a3f05d4329bb7fdfba0413a4e2ebb20aa28cd
WasathTheekshana <wasath.vt@gmail.com>
2024-12-25 15:44:00 +0530

    feat: ability file that contains all the user access levels

---
+++ src/abilities/ability.ts
@@ -0,0 +1,28 @@
+import {
+  AbilityBuilder,
+  createMongoAbility,
+  MongoAbility,
+  InferSubjects,
+  PureAbility,
+  defineAbility,
+} from "@casl/ability";
+import { IUser } from "../models/user.model";
+import { IPost } from "../models/post.model";
+
+export type Actions = "manage" | "create" | "read" | "update" | "delete";
+export type Subjects = InferSubjects<IPost | IUser> | "all";
+export type AppAbility = MongoAbility<[Actions, Subjects]>;
+
+export const defineAbilityFor = (user: IUser) => {
+  return defineAbility((can) => {
+    if (user.role === "admin") {
+      can("manage", "all");
+    } else {
+      // Regular users
+      can("read", "Post");
+      can(["create", "update", "delete"], "Post", { author: user._id });
+      can("read", "User", { _id: user._id });
+      can("update", "User", { _id: user._id });
+    }
+  });
+};
